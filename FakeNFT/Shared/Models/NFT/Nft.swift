40f73419cbc9bbd607122dee302844aba62885db
Nikita Volya <Deysyy88@gmail.com>
2026-03-27 17:15:33 +0300

    feat(api): added new field to struct nft to match parsed data

--- FakeNFT/Shared/Models/NFT/Nft.swift
+++ FakeNFT/Shared/Models/NFT/Nft.swift
@@ -3,4 +3,10 @@ import Foundation
 struct Nft: Decodable {
     let id: String
     let images: [URL]
+    let name: String
+    let price: Double
+    let rating: Int
+    let authorId: String
+    let description: String
+    let website: URL
 }
