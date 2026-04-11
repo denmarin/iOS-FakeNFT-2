b1b6b525a5996c289915c92649ac97c5e588da59
denmarin <Danya6846084@gmail.com>
2026-04-11 20:33:10 +0400

    fix: return duplicates because deduplication broke the sortby from api

--- FakeNFT/Features/Catalog/Services/DTO/CatalogCollectionDTO.swift
+++ FakeNFT/Features/Catalog/Services/DTO/CatalogCollectionDTO.swift
@@ -7,31 +7,4 @@ struct CatalogCollectionDTO: Decodable {
     let description: String
     let author: String
     let id: String
-
-    private enum CodingKeys: String, CodingKey {
-        case name
-        case cover
-        case nfts
-        case description
-        case author
-        case id
-    }
-
-    init(from decoder: Decoder) throws {
-        let container = try decoder.container(keyedBy: CodingKeys.self)
-        self.name = try container.decode(String.self, forKey: .name)
-        self.cover = try container.decode(String.self, forKey: .cover)
-        let rawNftIDs = try container.decode([String].self, forKey: .nfts)
-        self.nfts = rawNftIDs.uniquePreservingOrder()
-        self.description = try container.decode(String.self, forKey: .description)
-        self.author = try container.decode(String.self, forKey: .author)
-        self.id = try container.decode(String.self, forKey: .id)
-    }
-}
-
-private extension Array where Element: Hashable {
-    func uniquePreservingOrder() -> [Element] {
-        var seen: Set<Element> = []
-        return filter { seen.insert($0).inserted }
-    }
 }
